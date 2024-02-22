0b049d34b938fab6aa966c295f61a8623fd0c739
kuberjicn <kuberjicn@gmail.com>
2024-02-22 06:46:35 +0530

    leavereg

--- src/pages/Master/LeaveRegister.jsx
+++ src/pages/Master/LeaveRegister.jsx
@@ -5,7 +5,7 @@ import React, { useEffect, useState } from "react";
 import axios from "../../AxiosConfig";
 
 function LeaveRegister() {
-  // const employees=[{"name":"bhavesh vanecha","id":"10","year":"2024",'leave':[{"leavetype":"casual","opbal":"25","consumed":"10"},{"leavetype":"sick","opbal":"12","consumed":"4"}]},{"name":"sagar markana","id":"11","year":"2024",'leave':[{"leavetype":"casual","opbal":"25","consumed":"10"},{"leavetype":"sick","opbal":"12","consumed":"4"}]},,{"name":"sagar markana","id":"11","year":"2024",'leave':[{"leavetype":"casual","opbal":"25","consumed":"10"},{"leavetype":"sick","opbal":"12","consumed":"4"}]},,{"name":"sagar markana","id":"11","year":"2024",'leave':[{"leavetype":"casual","opbal":"25","consumed":"10"},{"leavetype":"sick","opbal":"12","consumed":"4"}]},,{"name":"sagar markana","id":"11","year":"2024",'leave':[{"leavetype":"casual","opbal":"25","consumed":"10"},{"leavetype":"sick","opbal":"12","consumed":"4"}]},,{"name":"sagar markana","id":"11","year":"2024",'leave':[{"leavetype":"casual","opbal":"25","consumed":"10"},{"leavetype":"sick","opbal":"12","consumed":"4"}]},,{"name":"sagar markana","id":"11","year":"2024",'leave':[{"leavetype":"casual","opbal":"25","consumed":"10"},{"leavetype":"sick","opbal":"12","consumed":"4"}]},,{"name":"sagar markana","id":"11","year":"2024",'leave':[{"leavetype":"casual","opbal":"25","consumed":"10"},{"leavetype":"sick","opbal":"12","consumed":"4"}]},,{"name":"sagar markana","id":"11","year":"2024",'leave':[{"leavetype":"casual","opbal":"25","consumed":"10"},{"leavetype":"sick","opbal":"12","consumed":"4"}]}]
+  
 
   const [data,setData]=useState([])
   const [error,setError]=useState('')
