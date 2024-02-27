fced6bb078cd084bd5e05a9054765a4e4b53071b
kuberjicn <kuberjicn@gmail.com>
2024-02-27 18:28:54 +0530

    2702E

--- src/component/AttandanceCard.jsx
+++ src/component/AttandanceCard.jsx
@@ -1,39 +1,92 @@
-import React from 'react'
+import React from "react";
 
-function AttandanceCard({fhtype,shtype,supname='xyz',intime='00.00.00',outtime='00.00.00'}) {
- 
+function AttandanceCard({
+  fhtype,
+  shtype,
+  supname = "xyz",
+  intime = "00.00.00",
+  outtime = "00.00.00",
+  toggleshtype,
+  togglefhtype,
+}) {
   return (
     <>
-    
-      <div className='card-header'>
-     
-                <h3 >{supname}</h3>
-            
-        <div className='att'>
-        <div className="card-attandance " >
-            <div className='att-header'>
-                <h3 >Morning</h3>
+      <div className="card-header">
+        <h3 title={supname}>{supname}</h3>
+
+        <div className="att">
+          <div className="card-attandance ">
+            <div className="att-header">
+              <h3>Morning</h3>
             </div>
-            <div className= {`pbtn text-center  ${ fhtype===1 ? 'pbtn-holiday' :fhtype===2 ? 'pbtn-absent' : fhtype===3 ? 'pbtn-present' : fhtype===4 ? 'pbtn-casual' : fhtype===5 && 'pbtn-sick' } `} onClick={()=>alert('dd')}>
-              {fhtype===1 ? 'H':fhtype===2?'A':fhtype===3?'P':fhtype===4?'L':fhtype===5&&'S'}
+            <div
+              className={`pbtn text-center  ${
+                fhtype === 1
+                  ? "pbtn-holiday"
+                  : fhtype === 2
+                  ? "pbtn-absent"
+                  : fhtype === 3
+                  ? "pbtn-present"
+                  : fhtype === 4
+                  ? "pbtn-casual"
+                  : fhtype === 5 && "pbtn-sick"
+              } `}
+              onClick={() => togglefhtype()}
+            >
+              {fhtype === 1
+                ? "H"
+                : fhtype === 2
+                ? "A"
+                : fhtype === 3
+                ? "P"
+                : fhtype === 4
+                ? "L"
+                : fhtype === 5 && "S"}
             </div>
+          </div>
+          <div className="card-attandance ">
+            <div className="att-header">
+              <h3>Evening</h3>
+            </div>
+            <div
+              className={` text-center pbtn ${
+                shtype === 1
+                  ? "pbtn-holiday"
+                  : shtype === 2
+                  ? "pbtn-absent"
+                  : shtype === 3
+                  ? "pbtn-present"
+                  : shtype === 4
+                  ? "pbtn-casual"
+                  : shtype === 5 && "pbtn-sick"
+              } `}
+              onClick={() => toggleshtype()}
+            >
+              {shtype === 1
+                ? "H"
+                : shtype === 2
+                ? "A"
+                : shtype === 3
+                ? "P"
+                : shtype === 4
+                ? "L"
+                : shtype === 5 && "S"}
+            </div>
+          </div>
         </div>
-        <div className="card-attandance " >
-            <div className='att-header'>
-                <h3 >Evening</h3>
-            </div>
-            <div className= {` text-center pbtn ${ shtype===1 ? 'pbtn-holiday' :shtype===2 ? 'pbtn-absent' : shtype===3 ? 'pbtn-present' : shtype===4 ? 'pbtn-casual' : shtype===5 && 'pbtn-sick' } `} onClick={()=>alert('dd')}>
-            {shtype===1 ? 'H':shtype===2?'A':shtype===3?'P':shtype===4?'L':shtype===5&&'S'}
-            </div>
+        <div className="card-footer">
+          <div className="disp-time">
+            <span> in-time :</span>
+            {intime}
+          </div>
+          <div className="disp-time">
+            <span>out-time:</span>
+            {outtime}
+          </div>
         </div>
-      </div>
-      <div className='card-footer'>
-        <div className='disp-time'><span>intime :</span>{intime}</div>
-        <div className='disp-time'><span>outtime:</span>{outtime}</div>
-      </div>
       </div>
     </>
-  )
+  );
 }
 
-export default AttandanceCard
+export default AttandanceCard;
--- src/pages/Attaandance/AttandanceRegister.jsx
+++ src/pages/Attaandance/AttandanceRegister.jsx
@@ -1,28 +1,65 @@
-import React from 'react'
-import AttandanceCard from '../../component/AttandanceCard'
-
+import React, { useEffect, useState } from "react";
+import AttandanceCard from "../../component/AttandanceCard";
+import BusyForm from "../../component/BusyForm";
+import axios from "../../AxiosConfig";
+import TitalBar from "../../component/TitalBar";
 function AttandanceRegister() {
+  const [data, setData] = useState([]);
+  const [isBusyShow, setIsBusyShow] = useState(false);
+  const getData = async () => {
+    setIsBusyShow(true);
+
+    await axios
+      .get(`/attendance/?att_date=2023-08-30`)
+
+      .then((response) => {
+        setData(response.data.results);
+        console.log(response.data);
+      })
+      .catch(() => {
+        setIsBusyShow(false);
+
+        setError("Something went wrong. Please try again later.");
+      });
+    setIsBusyShow(false);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const toggleShtype = (attid, atttype) => {
+    console.log("shtype", attid, atttype);
+  };
+
+  const toggleFhtype = (attid, atttype) => {
+    console.log("fhtype", attid, atttype);
+  };
   return (
-    <div className='grid-attandance' >
-      
-      <AttandanceCard fhtype={3} shtype={4}/>
-      <AttandanceCard fhtype={1} shtype={3}/>
-      <AttandanceCard fhtype={2} shtype={2}/>
-      <AttandanceCard fhtype={5} shtype={3}/>
-      <AttandanceCard fhtype={4} shtype={2}/>
-      <AttandanceCard fhtype={1} shtype={2}/>
-      <AttandanceCard fhtype={2} shtype={2}/>
-      <AttandanceCard fhtype={2} shtype={2}/>
-      <AttandanceCard fhtype={3} shtype={4}/>
-      <AttandanceCard fhtype={1} shtype={3}/>
-      <AttandanceCard fhtype={2} shtype={2}/>
-      <AttandanceCard fhtype={5} shtype={3}/>
-      <AttandanceCard fhtype={4} shtype={2}/>
-      <AttandanceCard fhtype={1} shtype={2}/>
-      <AttandanceCard fhtype={2} shtype={2}/>
-      <AttandanceCard fhtype={2} shtype={2}/>
-    </div>
-  )
+    <>
+      <BusyForm isShow={isBusyShow} />
+      <TitalBar
+        addvisible={false}
+        isVisible="YearSelector"
+        title="Employee LIst of Year :"
+        initialvalue={""}
+        onRefresh={() => getData()}
+      />
+      <div className="grid-attandance">
+        {data.map((item) => (
+          <AttandanceCard
+            key={item.att_id}
+            fhtype={item.fhType.typ_id}
+            shtype={item.shType.typ_id}
+            supname={item.supid.sup_name}
+            intime={item.intime}
+            outtime={item.outtime}
+            togglefhtype={() => toggleFhtype(item.att_id, item.fhType.typ_id)}
+            toggleshtype={() => toggleShtype(item.att_id, item.shType.typ_id)}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default AttandanceRegister
+export default AttandanceRegister;
